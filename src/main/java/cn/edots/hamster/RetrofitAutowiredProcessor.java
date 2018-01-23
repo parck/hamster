8840f3699dcf333fa6669b69594f162458cb6908
parck <parcklee163.com>
2018-01-23 12:42:00 +0800

    commit on 2018/01/23

--- src/main/java/cn/edots/hamster/RetrofitAutowiredProcessor.java
+++ src/main/java/cn/edots/hamster/RetrofitAutowiredProcessor.java
@@ -1,17 +1,14 @@
 package cn.edots.hamster;
 
 import cn.edots.hamster.annotation.API;
-import cn.edots.hamster.annotation.APIResource;
+import cn.edots.hamster.annotation.Inject;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.util.ReflectionUtils;
-import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.lang.reflect.Field;
-import java.util.logging.Logger;
 
 public class RetrofitAutowiredProcessor extends InstantiationAwareBeanPostProcessorAdapter {
 
@@ -24,10 +21,8 @@ public class RetrofitAutowiredProcessor extends InstantiationAwareBeanPostProces
         ReflectionUtils.doWithFields(bean.getClass(), new ReflectionUtils.FieldCallback() {
 
             public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
-                APIResource apiResource = field.getAnnotation(APIResource.class);
-                Resource resource = field.getAnnotation(Resource.class);
-                Autowired autowired = field.getAnnotation(Autowired.class);
-                if (apiResource == null) if (resource == null) if (autowired == null) return;
+                Inject inject = field.getAnnotation(Inject.class);
+                if (inject == null) return;
                 //读取注解中的值
                 API api = field.getType().getAnnotation(API.class);
                 if (api == null) return;
