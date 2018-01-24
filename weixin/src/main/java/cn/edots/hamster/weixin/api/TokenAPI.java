71d4d2d796e12bc895e928763a9b9bf2509b7e7b
parck <parcklee163.com>
2018-01-24 11:13:02 +0800

    commit on 2018/01/23

--- weixin/src/main/java/cn/edots/hamster/weixin/api/TokenAPI.java
+++ weixin/src/main/java/cn/edots/hamster/weixin/api/TokenAPI.java
@@ -4,21 +4,34 @@ import cn.edots.hamster.core.annotation.API;
 import cn.edots.hamster.weixin.api.result.AccessResult;
 import io.reactivex.Observer;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 @API
-public interface TokenAPI {
+public abstract class TokenAPI {
 
     /**
      * @param appId  appId
      * @param secret secret
-     * @return AccessResult
+     * @return Call<AccessResult>
      */
     @GET("/cgi-bin/token?grant_type=client_credential")
-    void access(@Query("appid") String appId, @Query("secret") String secret, Observer<AccessResult> observer);
+    public abstract Call<AccessResult> access(@Query("appid") String appId, @Query("secret") String secret);
 
-    @GET("/cgi-bin/token?grant_type=client_credential")
-    Call<AccessResult> access(@Query("appid") String appId, @Query("secret") String secret);
+    public void access(String appId, String secret, final Observer<AccessResult> observer) {
+        access(appId, secret).enqueue(new Callback<AccessResult>() {
+            public void onResponse(Call<AccessResult> call, Response<AccessResult> response) {
+                observer.onNext(response.body());
+                observer.onComplete();
+            }
+
+            public void onFailure(Call<AccessResult> call, Throwable throwable) {
+                observer.onError(throwable);
+                observer.onComplete();
+            }
+        });
+    }
 
 }
