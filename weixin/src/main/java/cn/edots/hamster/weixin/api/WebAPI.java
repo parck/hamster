f85980d4aafe4de279ad3859a4943446d604b4e0
parck <parcklee163.com>
2018-01-24 11:59:14 +0800

    commit on 2018/01/23

--- weixin/src/main/java/cn/edots/hamster/weixin/api/WebAPI.java
+++ weixin/src/main/java/cn/edots/hamster/weixin/api/WebAPI.java
@@ -11,5 +11,5 @@ import retrofit2.http.Query;
 public interface WebAPI {
 
     @GET("/connect/oauth2/authorize?response_type=code&state={state}#wechat_redirect")
-    Call<Result> authorize(@Query("appid") String appId, @Query("redirect_uri") String redirectURI, @Query("scope") String scope, @Path("state") String state);
+    Call<Result> authorize(@Query("appid") String appId, @Query("redirect_uri") String redirectURI, @Query("scope") String scope, @Query("state") String state);
 }
