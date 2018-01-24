5eee3d7614da18e22730a1879872685dcfb73d1c
parck <parcklee163.com>
2018-01-24 10:36:31 +0800

    commit on 2018/01/23

--- weixin/src/main/java/cn/edots/hamster/weixin/api/TokenAPI.java
+++ weixin/src/main/java/cn/edots/hamster/weixin/api/TokenAPI.java
@@ -1,7 +1,6 @@
 package cn.edots.hamster.weixin.api;
 
 import cn.edots.hamster.core.annotation.API;
-import cn.edots.hamster.weixin.api.param.GrantType;
 import cn.edots.hamster.weixin.api.result.AccessResult;
 import io.reactivex.Observable;
 import retrofit2.http.GET;
@@ -13,9 +12,8 @@ public interface TokenAPI {
     /**
      * @param appId     appId
      * @param secret    secret
-     * @param grantType GrantType
      * @return AccessResult
      */
-    @GET("/cgi-bin/token")
-    Observable<AccessResult> access(@Query("appid") String appId, @Query("secret") String secret, @Query("grant_type") GrantType grantType);
+    @GET("/cgi-bin/token?grant_type=client_credential")
+    Observable<AccessResult> access(@Query("appid") String appId, @Query("secret") String secret);
 }
