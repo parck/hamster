290b75b9012405aa77418b6f1ebade49ba0b3967
parck <parcklee@163.com>
2018-04-09 10:51:39 +0800

    2018/04/09.

--- alibaba/src/main/java/cn/edots/hamster/alibaba/oss/AliOSS.java
+++ alibaba/src/main/java/cn/edots/hamster/alibaba/oss/AliOSS.java
@@ -9,31 +9,36 @@ import java.net.URL;
 import java.text.ParseException;
 import java.util.Date;
 
-public class OSS {
+public class AliOSS {
 
-    protected static final String OSS_CN_HANGZHOU = "oss-cn-hangzhou.aliyuncs.com";
-    protected static final String OSS_CN_SHANGHAI = "oss-cn-shanghai.aliyuncs.com";
-    protected static final String OSS_CN_QINGDAO = "oss-cn-qingdao.aliyuncs.com";
-    protected static final String OSS_CN_BEIJING = "oss-cn-beijing.aliyuncs.com";
-    protected static final String OSS_CN_ZHANGJIAKOU = "oss-cn-zhangjiakou.aliyuncs.com";
-    protected static final String OSS_CN_HUHEHAOTE = "oss-cn-huhehaote.aliyuncs.com";
-    protected static final String OSS_CN_SHENZHEN = "oss-cn-shenzhen.aliyuncs.com";
-    protected static final String OSS_CN_HONGKONG = "oss-cn-hongkong.aliyuncs.com";
-    protected static final String OSS_US_WEST_1 = "oss-us-west-1.aliyuncs.com";
-    protected static final String OSS_US_EAST_1 = "oss-us-east-1.aliyuncs.com";
-    protected static final String OSS_AP_SOUTHEAST_1 = "oss-ap-southeast-1.aliyuncs.com";
-    protected static final String OSS_AP_SOUTHEAST_2 = "oss-ap-southeast-2.aliyuncs.com";
-    protected static final String OSS_AP_SOUTHEAST_3 = "oss-ap-southeast-3.aliyuncs.com";
-    protected static final String OSS_AP_NORTHEAST_1 = "oss-ap-northeast-1.aliyuncs.com";
-    protected static final String OSS_AP_SOUTH_1 = "oss-ap-south-1.aliyuncs.com";
-    protected static final String OSS_EU_CENTRAL_1 = "oss-eu-central-1.aliyuncs.com";
-    protected static final String OSS_ME_EAST_1 = "oss-me-east-1.aliyuncs.com";
-    
+    public static final String OSS_CN_HANGZHOU = "oss-cn-hangzhou.aliyuncs.com";
+    public static final String OSS_CN_SHANGHAI = "oss-cn-shanghai.aliyuncs.com";
+    public static final String OSS_CN_QINGDAO = "oss-cn-qingdao.aliyuncs.com";
+    public static final String OSS_CN_BEIJING = "oss-cn-beijing.aliyuncs.com";
+    public static final String OSS_CN_ZHANGJIAKOU = "oss-cn-zhangjiakou.aliyuncs.com";
+    public static final String OSS_CN_HUHEHAOTE = "oss-cn-huhehaote.aliyuncs.com";
+    public static final String OSS_CN_SHENZHEN = "oss-cn-shenzhen.aliyuncs.com";
+    public static final String OSS_CN_HONGKONG = "oss-cn-hongkong.aliyuncs.com";
+    public static final String OSS_US_WEST_1 = "oss-us-west-1.aliyuncs.com";
+    public static final String OSS_US_EAST_1 = "oss-us-east-1.aliyuncs.com";
+    public static final String OSS_AP_SOUTHEAST_1 = "oss-ap-southeast-1.aliyuncs.com";
+    public static final String OSS_AP_SOUTHEAST_2 = "oss-ap-southeast-2.aliyuncs.com";
+    public static final String OSS_AP_SOUTHEAST_3 = "oss-ap-southeast-3.aliyuncs.com";
+    public static final String OSS_AP_NORTHEAST_1 = "oss-ap-northeast-1.aliyuncs.com";
+    public static final String OSS_AP_SOUTH_1 = "oss-ap-south-1.aliyuncs.com";
+    public static final String OSS_EU_CENTRAL_1 = "oss-eu-central-1.aliyuncs.com";
+    public static final String OSS_ME_EAST_1 = "oss-me-east-1.aliyuncs.com";
+
     protected String bucket;
-    protected final OSSClient client;
+    protected OSSClient client;
 
-    public OSS(String endpoint, String key, String secret, String bucket) {
-        client = new OSSClient(endpoint, key, secret);
+    public AliOSS(OSSClient client, String bucket) {
+        this.client = client;
+        this.bucket = bucket;
+    }
+
+    public AliOSS(String endpoint, String key, String secret, String bucket) {
+        this.client = new OSSClient(endpoint, key, secret);
         this.bucket = bucket;
     }
 
@@ -77,7 +82,7 @@ public class OSS {
         return client.putObject(bucket, key, new FileInputStream(file), meta).getETag();
     }
 
-    public String getURL(String key) throws ParseException {
+    public String getURL(String key) {
         Date expires = new Date(new Date().getTime() + 1000 * 60 * 60 * 24);
         GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucket, key);
         generatePresignedUrlRequest.setExpiration(expires);
